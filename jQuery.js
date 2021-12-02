1a53ab5cdc8b560127e37d99b147b005e1afa3ac
project3years <githubproject3years@gmail.com>
2021-12-02 09:06:37 +0500

    This is my website

---
+++ jQuery.js
@@ -0,0 +1,155 @@
+// IS NOT WORKING
+jQuery.noConflict();
+jQuery(document).ready(function($){});
+// =========================================
+
+
+            $("#navbar").mouseover(function(){
+                           
+                $("#navbar").css({"background-color": "#424242"});
+                
+            });
+            $("#navbar").mouseleave(function(){
+        
+                $("#navbar").css({"background-color": "transparent"});
+                
+            });
+            
+            
+        
+        $("#btn").mouseover(function(){
+            $("#arrow").animate(
+                {left:"+=6"},250)  });
+                $("#btn").mouseleave(function(){
+                    $("#arrow").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+
+
+        $("#btn1").mouseover(function(){
+            $("#card1").animate(
+                {left:"+=6"},250)  });
+                $("#btn1").mouseleave(function(){
+                    $("#card1").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+        $("#btn2").mouseover(function(){
+            $("#card2").animate(
+                {left:"+=6"},250)  });
+                $("#btn2").mouseleave(function(){
+                    $("#card2").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+        $("#btn3").mouseover(function(){
+            $("#card3").animate(
+                {left:"+=6"},250)  });
+                $("#btn3").mouseleave(function(){
+                    $("#card3").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+        $("#btn4").mouseover(function(){
+            $("#card4").animate(
+                {left:"+=6"},250)  });
+                $("#btn4").mouseleave(function(){
+                    $("#card4").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+        $("#btn5").mouseover(function(){
+            $("#card5").animate(
+                {left:"+=6"},250)  });
+                $("#btn5").mouseleave(function(){
+                    $("#card5").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+        $("#btn6").mouseover(function(){
+            $("#card6").animate(
+                {left:"+=6"},250)  });
+                $("#btn6").mouseleave(function(){
+                    $("#card6").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+        $("#btn7").mouseover(function(){
+            $("#card7").animate(
+                {left:"+=6"},250)  });
+                $("#btn7").mouseleave(function(){
+                    $("#card7").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+        $("#btn8").mouseover(function(){
+            $("#card8").animate(
+                {left:"+=6"},250)  });
+                $("#btn8").mouseleave(function(){
+                    $("#card8").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+        $("#btn9").mouseover(function(){
+            $("#card9").animate(
+                {left:"+=6"},250)  });
+                $("#btn9").mouseleave(function(){
+                    $("#card9").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+        $("#btn10").mouseover(function(){
+            $("#card10").animate(
+                {left:"+=6"},250)  });
+                $("#btn10").mouseleave(function(){
+                    $("#card10").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+        $("#btn11").mouseover(function(){
+            $("#card11").animate(
+                {left:"+=6"},250)  });
+                $("#btn11").mouseleave(function(){
+                    $("#card11").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+        $("#btn12").mouseover(function(){
+            $("#card12").animate(
+                {left:"+=6"},250)  });
+                $("#btn12").mouseleave(function(){
+                    $("#card12").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+        $("#btn13").mouseover(function(){
+            $("#card13").animate(
+                {left:"+=6"},250)  });
+                $("#btn13").mouseleave(function(){
+                    $("#card13").animate(
+                        {left:"-=6"},250)
+      
+                    
+        });
+            $(window).scroll(function(){
+                $("#navbar").css({"background-color": "#424242"});
+                
+            });
+        $('.carousel').carousel({
+                interval: 400
+              })
+
